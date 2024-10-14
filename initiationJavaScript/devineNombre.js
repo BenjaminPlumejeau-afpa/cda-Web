fbcd7d6c0ae0452a5e1f5999d49e807bb697e53b
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2024-10-14 16:05:19 +0200

    Amélioration de devineNombre

--- initiationJavaScript/devineNombre.js
+++ initiationJavaScript/devineNombre.js
@@ -1,23 +1,25 @@
-// A activer dans la page html - fichier désactivé car trop obnoxious pour tester les autres exercices
-
 // Fonction permettant de trouver le nombre de l'utilisateur
 function devineNombre(min, actuel, max) {
     let reponse = prompt(actuel + ". + (Plus grand), - (Plus petit), = (Trouvé !)");
 
-    if (reponse === "+") {
-        if (actuel * 2 > max) {
-            if (actuel + 5 > max) {
-                return devineNombre(actuel, actuel + 1, max);
+    if ((reponse !== "+") && (reponse !== "-") && (reponse !== "=")) {
+        throw new Error("Erreur de saisie - Mauvais opérateur ('" + reponse + "')");
+    } else {
+        if (reponse === "+") {
+            if (actuel >= max - 1) {
+                alert("Trouvé ! Votre nombre était " + max);
             } else {
-                return devineNombre(actuel, actuel + 5, max);
+                devineNombre(actuel, Math.trunc((actuel + max) / 2), max);
+            }
+        } else if (reponse === "-") {
+            if (actuel <= min + 1) {
+                alert("Trouvé ! Votre nombre était " + min);
+            } else {
+                devineNombre(min, Math.trunc((actuel + min) / 2), actuel);
             }
         } else {
-            return devineNombre(actuel, actuel * 2, max);
+            alert("Trouvé ! Votre nombre était " + actuel);
         }
-    } else if (reponse === "-") {
-        return devineNombre(min, Math.trunc((actuel + min) / 2), actuel);
-    } else {
-        alert("Trouvé !");
     }
 }
 
@@ -27,5 +29,10 @@ if (saisie === "o") {
     let min = 0;
     let max = 100;
     alert("Choisissez un nombre entre " + min + " et " + max + ", et retenez le bien !");
-    devineNombre(min, Math.trunc(max / 2, max));
+
+    try {
+        devineNombre(min, Math.trunc(max / 2, max), max);
+    } catch (e) {
+        console.error(e);
+    }
 }
